f540ae9a1319281b7588b364e50f807c9000171e
Mouhamad-Shaker-1 <mouhamadshaker073@gmail.com>
2024-09-23 12:14:43 -0700

    added security

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,13 @@
 require('dotenv').config()
 require('express-async-errors'); 
 
+// Security 
+
+const helmet = require('helmet')
 const cors = require('cors');
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express');   
 const path = require('path');
 const app = express();
@@ -16,11 +22,17 @@ const authMiddleware = require('./middleware/authentication')
 const userRouter = require('./routes/user')
 const tasksRouter = require('./routes/tasks')
 
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, 
+}))
+app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 app.use('/uploads', express.static(__dirname + '/uploads'))
-app.use(cors())
-app.use(express.json())
-
 
 app.use('/api/v1/auth', userRouter)
 app.use('/api/v1/tasks', authMiddleware, tasksRouter)
